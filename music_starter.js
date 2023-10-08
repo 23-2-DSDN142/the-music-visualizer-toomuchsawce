a98b73baa01aed184b473aaeac66046c6ee8d94c
toomuchsawce <140985963+toomuchsawce@users.noreply.github.com>
2023-10-09 10:53:54 +1300

    1st commit

--- music_starter.js
+++ music_starter.js
@@ -1,43 +1,39 @@
-
+let firstRun = true
+let base;
 // vocal, drum, bass, and other are volumes ranging from 0 to 100
-function draw_one_frame(words, vocal, drum, bass, other, counter) {
-  background(20)
-  textFont('Helvetica'); // please use CSS safe fonts
-  rectMode(CENTER)
-  textSize(24);
+function draw_one_frame(vocal, drum, bass, other, counter) {
+    background(203, 94, 61);
+    rectMode(CENTER);
+    colorMode(RGB);
 
-   let bar_spacing = height / 10;
-   let bar_height = width / 12;
-   let bar_pos_x = width / 2;
- 
+    if (firstRun) {
+      base = loadImage('bubbleislandcloud.png');
+      animals = loadImage('animals.png');
 
-   // vocal bar is red
-   fill(200, 0, 0);
-   rect(bar_pos_x, height / 2 + 1 * bar_spacing, 4 * vocal, bar_height);
-   fill(0);
-   text("vocals", bar_pos_x, height / 2 + 1 * bar_spacing + 8);
- 
-   // drum bar is green
-   fill(0, 200, 0);
-   rect(bar_pos_x, height / 2 + 2 * bar_spacing, 4 * drum, bar_height);
-   fill(0);
-   text("drums", bar_pos_x, height / 2 + 2 * bar_spacing + 8);
- 
-   // bass bar is blue
-   fill(50, 50, 240);
-   rect(bar_pos_x, height / 2 + 3 * bar_spacing, 4 * bass, bar_height);
-   fill(0);
-   text("bass", bar_pos_x, height / 2 + 3 * bar_spacing + 8);
- 
-   // other bar is white
-   fill(200, 200, 200);
-   rect(bar_pos_x, height / 2 + 4 * bar_spacing, 4 * other, bar_height);
-   fill(0);
-   text("other", bar_pos_x, height / 2 + 4 * bar_spacing + 8);
-   fill(255, 255, 0);
- 
-   // display "words"
-   textAlign(CENTER);
-   textSize(vocal);
-   text(words, width/2, height/3);
-}
+      firstRun = false
+    }
+
+
+  // draw animals
+  push();
+  image(animals, -60, -0);
+  pop();
+
+  // draw island base layer
+  push();
+  image(base, -60, 0);
+  pop();
+
+
+
+  let bassMap = map(bass, 0, 100, 0, 60);
+
+
+  }  
+
+
+
+
+
+
+
